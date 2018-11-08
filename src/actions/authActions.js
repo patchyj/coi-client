0399be7ec63888f3290af60346313540007bb8de
patchyj <jackjwmcgregor@gmail.com>
2018-11-08 16:32:01 +0000

    trying to link the db

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -12,7 +12,7 @@ import Auth from "../validation/Auth.js";
 const config = {
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": `${process.env.PUBLIC_URL}/coi-client`,
+    "Access-Control-Allow-Origin": `https://patchyj.github.io/coi-client`,
     "Access-Control-Allow-Credentials": "true"
   }
 };

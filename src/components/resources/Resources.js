af693bc0c392f0534b14a2a445a5a9c7d18774de
patchyj <jackjwmcgregor@gmail.com>
2018-12-04 16:58:26 +0000

    resources page almost there

--- src/components/resources/Resources.js
+++ src/components/resources/Resources.js
@@ -94,30 +94,43 @@ class Resources extends Component {
     });
     return (
       <div className="resources">
-        <div className="jumbotron jumbotron-fluid red-sections">
+        <div className="background" />
+        <div className="background-text">
+          <h1>
+            <span className="display-4">Welcome to The Circle!</span>
+          </h1>
+          <p className="p-responsive">
+            Welcome to the resource page! Here you will find all the information
+            you need on The Circle of Young Intrapreneurs. We want you to know
+            every thing there is to know about us so if there is any thing you
+            can’t find here, please do contact us for more information.
+          </p>
+        </div>
+        <div className="jumbotron jumbotron-fluid">
           <div className="container py-5">
-            <h1>
-              <span className="display-4">Welcome to The Circle!</span>
-            </h1>
             <p className="p-responsive">
-              Welcome to the resource page! Here you will find all the
-              information you need on The Circle of Young Intrapreneurs. We want
-              you to know every thing there is to know about us so if there is
-              any thing you can’t find here, please do contact us for more
-              information.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
         </div>
-        <div className="container py-5">
-          {/* NAV */}
-          <nav>
-            <div className="nav nav-tabs" id="nav-tab" role="tablist">
-              {tabs}
+        <div className="container-fluid py-5 bg-main-white">
+          <div className="container">
+            {/* NAV */}
+            <nav>
+              <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                {tabs}
+              </div>
+            </nav>
+            {/* CONTENT */}
+            <div className="tab-content" id="nav-tabContent">
+              {panels}
             </div>
-          </nav>
-          {/* CONTENT */}
-          <div className="tab-content" id="nav-tabContent">
-            {panels}
           </div>
         </div>
       </div>

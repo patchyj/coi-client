48a94b6a66877bb7a039575b39c46c045f372399
patchyj <jackjwmcgregor@gmail.com>
2018-12-23 22:18:32 +0000

    trying to deploy

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
+axios.defaults.baseURL = "https://coi-node-api.herokuapp.com";
 
 const setAuthToken = token => {
   if (token) {
     // Apply to every request
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
   } else {
     // Delete auth header
-    delete axios.defaults.headers.common['Authorization'];
+    delete axios.defaults.headers.common["Authorization"];
   }
 };
 

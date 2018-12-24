f0257296e273595528a4daee19241aa7158af36f
patchyj <jackjwmcgregor@gmail.com>
2018-12-24 10:10:46 +0000

    added CORS

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -5,6 +5,7 @@ const setAuthToken = token => {
   if (token) {
     // Apply to every request
     axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
   } else {
     // Delete auth header
     delete axios.defaults.headers.common["Authorization"];

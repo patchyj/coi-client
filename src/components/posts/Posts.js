e86fcd513dce06318701ce04e345b53a80fc7405
patchyj <jackjwmcgregor@gmail.com>
2018-12-27 22:53:58 +0000

    wrapped the posts in a container

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -86,24 +86,26 @@ class Posts extends Component {
               <Link to="/posts/new">Have something to share?</Link>
             </h6>
           </div>
-          <div className="row posts-row p-3 m-3">
-            <div className="col-md-10">{allPosts}</div>
-            <div className="col-md-2">
-              <div className="sidenav">
-                <nav className="nav flex-column">
-                  <a className="nav-link active" target="_blank" href="#">
-                    <i className="fab fa-twitter" />
-                  </a>
-                  <a className="nav-link" target="_blank" href="#">
-                    <i className="fab fa-linkedin" />
-                  </a>
-                  <a className="nav-link" target="_blank" href="#">
-                    <i className="fab fa-facebook" />
-                  </a>
-                  <a className="nav-link" target="_blank" href="#">
-                    <i className="fab fa-instagram" />
-                  </a>
-                </nav>
+          <div className="container">
+            <div className="row posts-row p-3 m-3">
+              <div className="col-md-10">{allPosts}</div>
+              <div className="col-md-2">
+                <div className="sidenav">
+                  <nav className="nav flex-column">
+                    <a className="nav-link active" target="_blank" href="#">
+                      <i className="fab fa-twitter" />
+                    </a>
+                    <a className="nav-link" target="_blank" href="#">
+                      <i className="fab fa-linkedin" />
+                    </a>
+                    <a className="nav-link" target="_blank" href="#">
+                      <i className="fab fa-facebook" />
+                    </a>
+                    <a className="nav-link" target="_blank" href="#">
+                      <i className="fab fa-instagram" />
+                    </a>
+                  </nav>
+                </div>
               </div>
             </div>
           </div>

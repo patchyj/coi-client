4d80e44b4a10b8ea0dce2680849277e3304376b7
patchyj <jackjwmcgregor@gmail.com>
2018-12-31 15:13:41 +0000

    got the city finder working

--- src/components/auth/admin/AdminIndex.js
+++ src/components/auth/admin/AdminIndex.js
@@ -11,6 +11,13 @@ import { getProjects } from "../../../actions/projectActions";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Moment from "react-moment";
+// import LocationSearchInput from "../../chapters/NewChapterComponent";
+import PlacesAutocomplete from "react-places-autocomplete";
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng
+} from "react-places-autocomplete";
 
 const AdminCheckBox = ({ role, onChange, name, id }) => {
   let adminInput;
@@ -96,11 +103,20 @@ class AdminIndex extends Component {
       direction: {
         chapter_id: "asc"
       },
-      isAdmin: false
+      isAdmin: false,
+      address: "",
+      chapterName: {},
+      chapterCoords: {},
+      errors: {}
     };
 
     this.sortBy = this.sortBy.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  // handleChange(value) {
+  //   this.setState({ text: value });
+  // }
 
   sortBy(key) {
     this.setState({
@@ -173,6 +189,27 @@ class AdminIndex extends Component {
       }
     }
   }
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => {
+        let countryCode = results[0].formatted_address.split(",");
+        let chapter = {
+          city: countryCode[0].trim(),
+          country: countryCode[1].trim()
+        };
+        this.setState({ chapterName: chapter });
+      }) // returns a promise
+      .catch(error => this.setState({ error }));
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => this.setState({ chapterCoords: latLng }))
+      .catch(error => console.error("Error", error));
+  };
 
   componentDidMount() {
     const { user } = this.props.auth;
@@ -249,7 +286,10 @@ class AdminIndex extends Component {
                   <td>
                     {user.linkedinUrl ? (
                       <a href={`http://linkedin.com${user.linkedinUrl}`}>
-                        <i className="fab fa-linkedin" />
+                        <i
+                          className="fab fa-linkedin"
+                          style={{ color: "lime" }}
+                        />
                       </a>
                     ) : (
                       <i
@@ -261,7 +301,10 @@ class AdminIndex extends Component {
                   <td>
                     {user.twitterUrl ? (
                       <a href={`http://twitter.com${user.twitterUrl}`}>
-                        <i className="fab fa-twitter" />
+                        <i
+                          className="fab fa-twitter"
+                          style={{ color: "lime" }}
+                        />
                       </a>
                     ) : (
                       <i className="fab fa-twitter" style={{ color: "#aaa" }} />
@@ -322,6 +365,76 @@ class AdminIndex extends Component {
         <span className="jumbotron_h1 display-5 py-5 page-header">
           Wecome back, {this.props.auth.user.firstName}!
         </span>
+        <div className="jumbotron-fluid chapterForm p-5 text-center">
+          <a
+            className="text-center p-5"
+            data-toggle="collapse"
+            href="#newChapterForm"
+            role="button"
+            aria-expanded="false"
+            aria-controls="newChapterForm"
+          >
+            <h5 className="">
+              Add a chapter <i className="fas fa-chev-down" />
+            </h5>
+          </a>
+
+          <div className="collapse row" id="newChapterForm">
+            <div className="card card-body col-md-4 offset-md-4">
+              <form onSubmit={this.onSubmit}>
+                <PlacesAutocomplete
+                  value={this.state.address}
+                  onChange={this.handleChange}
+                  onSelect={this.handleSelect}
+                >
+                  {({
+                    getInputProps,
+                    suggestions,
+                    getSuggestionItemProps,
+                    loading
+                  }) => (
+                    <div>
+                      <input
+                        {...getInputProps({
+                          placeholder: "Search Places ...",
+                          className: "location-search-input"
+                        })}
+                      />
+                      <div className="autocomplete-dropdown-container">
+                        {loading && <div>Loading...</div>}
+                        {suggestions.map(suggestion => {
+                          const className = suggestion.active
+                            ? "suggestion-item--active"
+                            : "suggestion-item";
+                          // inline style for demonstration purpose
+                          const style = suggestion.active
+                            ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                            : { backgroundColor: "#ffffff", cursor: "pointer" };
+                          return (
+                            <div
+                              {...getSuggestionItemProps(suggestion, {
+                                className,
+                                style
+                              })}
+                            >
+                              <span>{suggestion.description}</span>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  )}
+                </PlacesAutocomplete>
+                <input
+                  type="submit"
+                  className="btn bg-main-red my-2"
+                  style={{ width: "100%" }}
+                  value="Create Chapter"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
 
         {results}
       </div>
